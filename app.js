be242389542c4e5f16858cff80e2df9bf04148ab
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-12-06 12:47:46 +0530

    cors added and returns the first response

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Product = require('./models/products');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 mongoose.connect("mongodb+srv://Charith:K7ulBusW5xqve3y0@cluster0-ow00d.mongodb.net/BudgetDB", {useNewUrlParser: true,  useUnifiedTopology: true})
     .then(() => {
-        console.log('connected successful');
+        console.log('connection Successful');
     }).catch(() => {
-    console.log('connection failed');
+        console.log('connection failed');
 });
 
 app.use((req, res, next) => {
     console.log('this is working');
-    res.send('hello from express');
+    next();
+});
+
+app.get('/products', (req, res, next) => {
+    const product = [{
+        id: '100d',
+        name: 'name1',
+        price: 20000,
+        imagePath: '/assets/image-assets/headphone.jpg',
+        category: 'electronics',
+        condition: 'new'
+    },
+        {
+            id: '100d',
+            name: 'name1',
+            price: 20000,
+            imagePath: '/assets/image-assets/headphone.jpg',
+            category: 'electronics',
+            condition: 'new'
+        },
+        {
+            id: '100d',
+            name: 'name1',
+            price: 20000,
+            imagePath: '/assets/image-assets/headphone.jpg',
+            category: 'electronics',
+            condition: 'new'
+        },
+        {
+            id: '100d',
+            name: 'name1',
+            price: 20000,
+            imagePath: '/assets/image-assets/headphone.jpg',
+            category: 'electronics',
+            condition: 'new'
+        },
+    ];
+
+    res.status(200).json({
+        message: 'success',
+        products: product
+    });
 });
 
 module.exports = app;
